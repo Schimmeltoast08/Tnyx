723318de3f5777e7b1c6b2838ddebd80c9889e91
Schimmeltoast08 <126913128+Schimmeltoast08@users.noreply.github.com>
2026-08-13 18:16:15 +0200

    init 2

--- tnyx/src/main/java/com/tnyx/Main.java
+++ tnyx/src/main/java/com/tnyx/Main.java
@@ -1,7 +1,82 @@
 package com.tnyx;
 
+import com.tnyx.util.Log;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.time.Instant;
+
 public class Main {
+
+    @SuppressWarnings("unused")
+    private static final int TNYX_MAIN_VERSION = 1;
+    private static final int VAULT_FORMAT_VERSION = 1;
+    private static final String KDF = "Argon2id"; // replace \w name later
+    private static final String ENCRYPTION_ALGORITHM = "AES"; // same as KDF
+    
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Log.log("Starting application", 2);
+        System.out.println(Instant.now().getEpochSecond());
+
+        if (args.length > 0 && args[0].equals("--new")) {
+            try {
+                Log.log("Creating new Vault", 2);
+                createOpenVault(args[1]);
+            } catch (IOException e) {
+                Log.log("Failed to create Vault", 4);
+            }
+        }
+
     }
+
+    private static void createOpenVault(String filepath) throws IOException {
+        try (FileOutputStream vaultOut = new FileOutputStream(filepath)) {
+
+            SecureRandom random = new SecureRandom();
+            //temporary
+            byte[] salt = new byte[16];
+            random.nextBytes(salt);
+            byte[] nonce = new byte[12];
+            random.nextBytes(nonce);
+            byte[] dek = new byte[32];
+            random.nextBytes(dek);
+            byte[] nonce2 = new byte[12];
+            random.nextBytes(nonce2); 
+
+            //   Write Block   \\
+            vaultOut.write("[Format]".getBytes(StandardCharsets.UTF_8));
+            vaultOut.write(VAULT_FORMAT_VERSION);
+            
+            vaultOut.write("[KDF]".getBytes(StandardCharsets.UTF_8));
+            vaultOut.write(KDF.getBytes(StandardCharsets.UTF_8).length);
+            vaultOut.write(KDF.getBytes(StandardCharsets.UTF_8));
+            vaultOut.write(salt.length);
+            vaultOut.write(salt);
+            //vaultOut.write(parameters);
+            
+            vaultOut.write("[Encryption]".getBytes(StandardCharsets.UTF_8));
+            vaultOut.write(ENCRYPTION_ALGORITHM.getBytes(StandardCharsets.UTF_8).length);
+            vaultOut.write(ENCRYPTION_ALGORITHM.getBytes(StandardCharsets.UTF_8));
+            vaultOut.write(nonce.length);
+            vaultOut.write(nonce);
+            // DEK u KEK, more research
+            //vaultOut.write(Data);
+            
+            vaultOut.write("[Time]".getBytes(StandardCharsets.UTF_8));
+            //vaultOut.write(Instant.now()); // creation date
+            vaultOut.write((byte) Instant.now().getEpochSecond()); // last modified. both equal during creation
+
+            vaultOut.write("[Data]".getBytes(StandardCharsets.UTF_8));
+            vaultOut.write(nonce2.length);
+            vaultOut.write(nonce2);
+
+
+            
+
+        }
+
+    }
+
 }
---
+++ tnyx/src/main/java/com/tnyx/util/Log.java
@@ -0,0 +1,119 @@
+package com.tnyx.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+public class Log {
+
+    @SuppressWarnings("unused")
+    private Log() {
+    } // so no instanciation
+
+    private static final String APP_NAME = "tnyx";
+
+    public static final Logger LOGGER = Logger.getLogger("TnyxLogger");
+    private static final String LOG_FILE_NAME = "tnyx.log";
+    private static final int MAX_LOG_SIZE = 5 * 1024 * 1024; // 5 MiB
+
+    static {
+        try {
+            initLogger();
+        } catch (Exception e) {
+            System.out.print("ERROR at initializing logger: " + e);
+        }
+    }
+
+    public static void log(String msg, int severity) {
+
+        Level severityLevel = Level.INFO;
+
+        switch (severity) {
+            case 0:
+                severityLevel = Level.OFF; break;
+            case 1:
+                severityLevel = Level.FINE; break;
+            case 2:
+                severityLevel = Level.INFO; break;
+            case 3:
+                severityLevel = Level.WARNING; break;
+            case 4:
+                severityLevel = Level.SEVERE; break;
+                
+
+            default:
+                severityLevel = Level.INFO; break;
+        }
+
+        LOGGER.log(severityLevel, msg);
+
+    }
+
+    private static String getLogPath() {
+        String userHome = System.getProperty("user.home");
+        String userOS = System.getProperty("os.name").toLowerCase().strip(); // strip just to be sure
+
+        File logDirectory;
+
+        if (userOS.contains("win")) { // Windows
+
+            String appData = System.getenv("APPDATA");
+
+            if (appData != null && !appData.isBlank()) {
+                logDirectory = new File(appData, APP_NAME);
+            } else {
+                logDirectory = new File(userHome, "AppData" + File.separator + "Roaming" + File.separator + APP_NAME);
+            }
+
+        } else if (userOS.contains("mac")) { // Mac
+            logDirectory = new File(userHome, "Library" + File.separator + "Logs" + File.separator + APP_NAME);
+        } else { // Linux
+            logDirectory = new File(userHome, ".local" + File.separator + "share" + File.separator + APP_NAME);
+        }
+
+        return new File(logDirectory, LOG_FILE_NAME).getAbsolutePath();
+    }
+
+    static private void initLogger() throws IOException {
+        String logPath = getLogPath();
+        File logFile = new File(logPath);
+
+        File logFileParent = logFile.getParentFile();
+
+        if (logFileParent != null && !logFileParent.exists()) {
+            if (!logFileParent.mkdirs() && !logFileParent.exists()) {
+                throw new IOException("Could not create log directory: " + logFileParent);
+            }
+        }
+
+        FileHandler fileHandler = new FileHandler(logPath, MAX_LOG_SIZE, 3, true); // 5 mb file, then log rotation, keep 3 max files, append
+
+        fileHandler.setFormatter(new Formatter() {
+            @Override
+            public String format(LogRecord record) {
+                return String.format("%1$tF %1$tT %2$s: %3$s%n", new java.util.Date(record.getMillis()),
+                        record.getLevel(),
+                        record.getMessage()
+                );
+            }
+        });
+
+        LOGGER.addHandler(fileHandler);
+        LOGGER.setLevel(Level.ALL);
+        LOGGER.setUseParentHandlers(false);
+        System.out.println(
+                "Tnyx log file: " + logFile.getAbsolutePath()
+        );
+
+    }
+
+    public static void log(String message, Level severity) {
+        LOGGER.log(severity, message);
+
+    }
+
+}
